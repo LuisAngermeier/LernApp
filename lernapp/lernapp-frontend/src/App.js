d291ff20678356001b829a3fdadb5cf3143e0fb6
Jose Chacin <jose.chacinn21@gmail.com>
2024-08-24 23:10:36 +0200

    Updated Footer

--- lernapp/lernapp-frontend/src/App.js
+++ lernapp/lernapp-frontend/src/App.js
@@ -1,26 +1,24 @@
-// src/App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import Footer from './components/Footer'; // Import Footer
-import Navbar from './components/Navbar'; // Import Navbar
+import Footer from './components/Footer'; 
+import Navbar from './components/Navbar'; 
 
 function App() {
     return (
         <Router>
             <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <Navbar /> {/* Add Navbar */}
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/posts/:fach" element={<Posts />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
-                <Footer /> {/* Add Footer */}
+                <Footer /> 
             </div>
         </Router>
     );
--- lernapp/lernapp-frontend/src/components/Footer.js
+++ lernapp/lernapp-frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 // src/components/Footer.js
 
-import React from 'react';
+/*import React from 'react';
 import { Container, Typography, Link } from '@mui/material';
 
 const Footer = () => {
@@ -26,6 +26,46 @@ const styles = {
         marginTop: 'auto',
         borderTop: '1px solid #e0e0e0',
     },
-};
+};*/
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faGithub, faReddit, faTelegram, faSnapchat, faGoogle, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import '../App.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024, LernApp. All rights reserved.</p>
+      <div className="social-icons">
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faXTwitter} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://github.com/LuisAngermeier/LernApp" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faReddit} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faTelegram} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faSnapchat} />
+        </a>
+        <a href="#" target="_self" rel="noopener noreferrer" className="social-icon">
+          <FontAwesomeIcon icon={faGoogle} />
+        </a>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
+
