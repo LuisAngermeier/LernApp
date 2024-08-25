5b85e1d3688d604501dff0f18f054e1f6ec6e742
Luis Angermeier <luis-mathias-johannes.angermeier@iu-study.org>
2024-08-25 22:22:29 +0200

    fix footer

--- lernapp/lernapp-frontend/src/pages/About.js
+++ lernapp/lernapp-frontend/src/pages/About.js
@@ -4,11 +4,16 @@ import { Container, Typography } from '@mui/material';
 const About = () => {
     return (
         <Container>
-            <Typography variant="h3" gutterBottom>
-                About Us
+            <Typography variant="h3" gutterBottom sx={{ marginTop: 4, marginBottom: 4 }}>
+                Über uns
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Wir sind ein Team von vier Studierenden der Internationalen Hochschule, bestehend aus Luis Angermeier, Jose Chacin, Nils Hornitschek und Stefan Arbutina.
+                Gemeinsam studieren wir den Kurs "Programmierung von Web-Anwendungen". Unser Ziel ist es, innovative und effiziente Webanwendungen zu entwickeln, die den Bedürfnissen der Nutzer gerecht werden.
             </Typography>
             <Typography variant="body1">
-                Welcome to the About Us page. Here you can provide information about your application or team.
+                Mit einer Leidenschaft für Technologie und einem starken Fokus auf praktische Anwendungen sind wir bestrebt, unser Wissen kontinuierlich zu erweitern und unsere Fähigkeiten in der Webentwicklung zu vertiefen.
+                Wir freuen uns darauf, unsere Kenntnisse in die Praxis umzusetzen und freuen uns, dass Sie mehr über uns erfahren möchten.
             </Typography>
         </Container>
     );
